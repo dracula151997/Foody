020dad33e26c73d7c1e40492837d2ef55196f45d
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-04-16 23:28:50 +0200

    create an entity for caching recipes and its DAO

---
+++ app/src/main/java/com/tutorial/foody/data/caching/RecipeEntity.kt
@@ -0,0 +1,14 @@
+package com.tutorial.foody.data.caching
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import com.tutorial.foody.models.FoodRecipeResponse
+
+@Entity(tableName = "recipes_table")
+class RecipeEntity(
+    var foodRecipe: FoodRecipeResponse
+) {
+
+    @PrimaryKey(autoGenerate = false)
+    var id: Int = 0
+}
---
+++ app/src/main/java/com/tutorial/foody/data/caching/RecipesDao.kt
@@ -0,0 +1,19 @@
+package com.tutorial.foody.data.caching
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.tutorial.foody.models.FoodRecipeResponse
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface RecipesDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRecipe(recipe: FoodRecipeResponse)
+
+    @Query("SELECT * FROM recipes_table ORDER BY id ASC")
+    suspend fun getCachingRecipes() : Flow<List<RecipeEntity>>
+
+
+}
