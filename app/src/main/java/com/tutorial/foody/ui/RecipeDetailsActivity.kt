408ac9e80efab1de30922c21a63b4aa56b20d8a8
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-04-18 23:00:16 +0200

    design Recipe details activity

---
+++ app/src/main/java/com/tutorial/foody/ui/RecipeDetailsActivity.kt
@@ -0,0 +1,11 @@
+package com.tutorial.foody.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RecipeDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe_details)
+    }
+}
