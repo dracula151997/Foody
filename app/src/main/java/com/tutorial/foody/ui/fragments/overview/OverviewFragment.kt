a8bc2e0633495f99a3176b9c7968f307ea21d891
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-04-23 11:27:55 +0200

    finishing Overview screen

--- app/src/main/java/com/tutorial/foody/ui/fragments/overview/OverviewFragment.kt
+++ app/src/main/java/com/tutorial/foody/ui/fragments/overview/OverviewFragment.kt
@@ -5,15 +5,20 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.tutorial.foody.R
 import com.tutorial.foody.databinding.FragmentOverviewBinding
+import com.tutorial.foody.models.RecipeResult
+import com.tutorial.foody.utils.Constants
 
 class OverviewFragment : Fragment() {
     private var _binding: FragmentOverviewBinding? = null
     private val binding get() = _binding!!
+    private var recipeData: RecipeResult? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            recipeData = it.getParcelable(Constants.RECIPE_RESULT)
+        }
     }
 
     override fun onCreateView(
@@ -23,6 +28,15 @@ class OverviewFragment : Fragment() {
         // Inflate the layout for this fragment
         _binding = FragmentOverviewBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    private fun setup() = with(binding) {
+        recipe = recipeData
     }
 
     override fun onDestroyView() {
