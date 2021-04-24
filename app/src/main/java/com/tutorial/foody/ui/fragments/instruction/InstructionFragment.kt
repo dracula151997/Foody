d29ab8bbf9c90a70b4a3362988493909b6c2228a
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-04-24 11:20:11 +0200

    show instructions url in web view

--- app/src/main/java/com/tutorial/foody/ui/fragments/instruction/InstructionFragment.kt
+++ app/src/main/java/com/tutorial/foody/ui/fragments/instruction/InstructionFragment.kt
@@ -4,20 +4,40 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.tutorial.foody.R
+import com.tutorial.foody.databinding.FragmentInstructionBinding
+import com.tutorial.foody.models.RecipeResult
+import com.tutorial.foody.utils.Constants
 
 class InstructionFragment : Fragment() {
-
+    private var _binding: FragmentInstructionBinding? = null
+    private val binding get() = _binding!!
+    private var result: RecipeResult? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        arguments?.let {
+            result = it.getParcelable(Constants.RECIPE_RESULT)
+        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_instruction, container, false)
+        _binding = FragmentInstructionBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    private fun setup() = with(binding){
+        instructionsWebView.webViewClient = WebViewClient()
+        result?.let { instructionsWebView.loadUrl(it.sourceUrl) }
     }
 }
