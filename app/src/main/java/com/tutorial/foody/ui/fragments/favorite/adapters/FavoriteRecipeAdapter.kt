e8250fb439dfed1d8f2d7c8ec8f301319c6feae8
Hassan Mohammed <hassan.s.mohammed1997@gmail.com>
2021-05-04 21:43:45 +0200

    setup contextual menu action with adapter

--- app/src/main/java/com/tutorial/foody/ui/fragments/favorite/adapters/FavoriteRecipeAdapter.kt
+++ app/src/main/java/com/tutorial/foody/ui/fragments/favorite/adapters/FavoriteRecipeAdapter.kt
@@ -1,17 +1,20 @@
 package com.tutorial.foody.ui.fragments.favorite.adapters
 
-import android.view.LayoutInflater
-import android.view.ViewGroup
+import android.view.*
+import androidx.fragment.app.FragmentActivity
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.tutorial.foody.R
 import com.tutorial.foody.data.database.entities.FavoriteRecipeEntity
 import com.tutorial.foody.databinding.FavoriteRecipeRowLayoutBinding
 import com.tutorial.foody.ui.DiffUtils
 import com.tutorial.foody.ui.fragments.favorite.FavoriteRecipesFragmentDirections
 
-class FavoriteRecipeAdapter :
-    RecyclerView.Adapter<FavoriteRecipeAdapter.FavoriteRecipeViewHolder>() {
+class FavoriteRecipeAdapter(
+    private val requireActivity: FragmentActivity
+) :
+    RecyclerView.Adapter<FavoriteRecipeAdapter.FavoriteRecipeViewHolder>(), ActionMode.Callback {
 
     private var favoriteRecipes = emptyList<FavoriteRecipeEntity>()
 
@@ -25,12 +28,19 @@ class FavoriteRecipeAdapter :
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoriteRecipeViewHolder =
         FavoriteRecipeViewHolder.from(parent)
 
-    override fun onBindViewHolder(holder: FavoriteRecipeViewHolder, position: Int) =
+    override fun onBindViewHolder(holder: FavoriteRecipeViewHolder, position: Int) {
         holder.bind(favoriteRecipes[position])
+        holder.itemView.setOnLongClickListener {
+            requireActivity.startActionMode(this)
+            true
+        }
+    }
 
     override fun getItemCount(): Int = favoriteRecipes.size
 
-    class FavoriteRecipeViewHolder(val binding: FavoriteRecipeRowLayoutBinding) :
+    class FavoriteRecipeViewHolder(
+        val binding: FavoriteRecipeRowLayoutBinding
+    ) :
         RecyclerView.ViewHolder(binding.root) {
 
         fun bind(favoriteRecipe: FavoriteRecipeEntity) {
@@ -57,5 +67,21 @@ class FavoriteRecipeAdapter :
             }
         }
 
+    }
+
+    override fun onCreateActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        mode?.menuInflater?.inflate(R.menu.favorite_contextual_menu, menu)
+        return true
+    }
+
+    override fun onPrepareActionMode(mode: ActionMode?, menu: Menu?): Boolean {
+        return true
+    }
+
+    override fun onActionItemClicked(mode: ActionMode?, item: MenuItem?): Boolean {
+        return true
+    }
+
+    override fun onDestroyActionMode(mode: ActionMode?) {
     }
 }
